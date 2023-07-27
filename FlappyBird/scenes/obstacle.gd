a2c341b01cc5c7b31cc2589db7254911c426ffc9
Sean Warne <sean.andrew.warne@gmail.com>
2023-07-27 10:31:08 -0600

    add single obstacle spawner

--- FlappyBird/scenes/obstacle.gd
+++ FlappyBird/scenes/obstacle.gd
@@ -2,18 +2,21 @@ extends Node2D
 
 const SPEED = -170
 
-func _physics_process(delta):
-	position.x += SPEED * delta
-	
-	if global_position.x <= -200:
-		global_position.x = 600
-		global_position.y = randi_range(210, 470)
+func _on_ready():
+	pass
 
+func _physics_process(delta):
+	move(delta)
+
+func move(delta):
+	print(position.x)
+	position.x += SPEED * delta
+	if global_position.x <= -200:
+		self.queue_free()
 
 func _on_obstacle_body_entered(body):
 	print("OBSTACLE HIT")
 	print(body)
-
 
 func _on_score_zone_body_exited(body):
 	print("SCORE!")
--- FlappyBird/scenes/world.gd
+++ FlappyBird/scenes/world.gd
@@ -3,13 +3,31 @@ extends Node2D
 @onready var player = $Player
 @onready var button = $Button
 
+@onready var obstacle = preload("res://scenes/obstacle.tscn")
+
+const OBSTACLE_MAX_HEIGHT = 720
+const OBSTACLE_MIN_HEIGHT = 100
+const OBSTACLE_DISTANCE   = 200
+const OBSTACLE_START_POS  = 500
+const MAX_OBSTACLES = 3
+
 func _ready():
-	pass
+	var obstacles = 0
 
 func _process(delta):
 	pass
 
-
 func _on_button_pressed():
 	player.start()
 	button.hide()
+	
+	spawn_obstacles()
+
+func spawn_obstacles():
+	var i = 0
+	
+	if i < 1:
+		var obstacle_instance = obstacle.instantiate()
+		add_child(obstacle_instance)
+		obstacle_instance.position.x = 600
+		i += 1
