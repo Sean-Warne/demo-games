366a8b7cdcd51fcb544e0604389814d1f387b249
Sean Warne <sean.andrew.warne@gmail.com>
2024-05-24 22:16:49 -0600

    player movement updated

--- ActionRpg/scripts/player.gd
+++ ActionRpg/scripts/player.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
 
-const SPEED = 140.0
+const MAX_SPEED = 100.0
+const ACCELERATION = 700
+const FRICTION = 1000
 
 func get_input(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity = Vector2.ZERO
 	
-	velocity.normalized()
+	if direction:
+		velocity += direction * ACCELERATION * delta
+		velocity = velocity.limit_length(MAX_SPEED)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 func _physics_process(delta):
 	get_input(delta)
