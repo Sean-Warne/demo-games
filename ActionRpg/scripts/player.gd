1fdad85a19954a56594310e3b913a4e826c557f3
Sean Warne <sean.andrew.warne@gmail.com>
2024-05-26 10:53:18 -0600

    updated player and bush collision

--- ActionRpg/scripts/player.gd
+++ ActionRpg/scripts/player.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 100.0
-const ACCELERATION = 700
-const FRICTION = 1000
+const MAX_SPEED = 80.0
+const ACCELERATION = 500
+const FRICTION = 700
+
+func _ready():
+	motion_mode = MOTION_MODE_FLOATING
 
 func get_input(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
+	print(direction)
+	
 	if direction:
-		velocity += direction * ACCELERATION * delta
-		velocity = velocity.limit_length(MAX_SPEED)
+		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
