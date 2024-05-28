6764d591155dd0cc206a399ee205b1d5c3cba9b9
Sean Warne <sean.andrew.warne@gmail.com>
2024-05-28 17:15:00 -0600

    update character walking animations

--- ActionRpg/scripts/player.gd
+++ ActionRpg/scripts/player.gd
@@ -4,17 +4,22 @@ const MAX_SPEED = 80.0
 const ACCELERATION = 500
 const FRICTION = 700
 
+@onready var animation_tree = $AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
+
 func _ready():
 	motion_mode = MOTION_MODE_FLOATING
 
 func get_input(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	print(direction)
-	
 	if direction:
+		animation_tree.set("parameters/Idle/blend_position", direction)
+		animation_tree.set("parameters/Run/blend_position", direction)
+		animation_state.travel("Run")
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 func _physics_process(delta):
