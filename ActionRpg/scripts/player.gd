a1222c4f730ca3e1a4f98c429e37a893aef4232e
Sean Warne <sean.andrew.warne@gmail.com>
2024-05-28 20:56:55 -0600

    created tilemap for dirt

--- ActionRpg/scripts/player.gd
+++ ActionRpg/scripts/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 80.0
-const ACCELERATION = 500
-const FRICTION = 700
+const ACCELERATION = 500.0
+const FRICTION = 700.0
 
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
+
+var speed = 80.0
 
 func _ready():
 	motion_mode = MOTION_MODE_FLOATING
@@ -13,11 +14,16 @@ func _ready():
 func get_input(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
+	if Input.is_action_pressed("sprint"):
+		speed = 120.0
+	else:
+		speed = 80.0
+	
 	if direction:
 		animation_tree.set("parameters/Idle/blend_position", direction)
 		animation_tree.set("parameters/Run/blend_position", direction)
 		animation_state.travel("Run")
-		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+		velocity = velocity.move_toward(direction * speed, ACCELERATION * delta)
 	else:
 		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
