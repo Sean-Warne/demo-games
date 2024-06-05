ff8e56bef529565a8ae2f072141bbcf0b92d45e2
Sean Warne <sean.andrew.warne@gmail.com>
2024-06-04 23:22:19 -0600

    add hurtbox and hitboxes for player and enemies; made grass static

--- ActionRpg/scripts/grass.gd
+++ ActionRpg/scripts/grass.gd
@@ -1,13 +1,11 @@
 extends Node2D
 
-func _ready():
-	pass # Replace with function body.
+func create_grass_effect():
+	var grass_effect = load("res://scenes/grass_effect.tscn").instantiate()
+	
+	get_parent().add_child(grass_effect)
+	grass_effect.position = self.position
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var grass_effect = load("res://scenes/grass_effect.tscn").instantiate()
-		
-		get_parent().add_child(grass_effect)
-		grass_effect.position = self.position
-		
-		queue_free()
+func _on_hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
