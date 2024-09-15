aee5fee8ce67cc3d8b07b22c45171c8f5405adf2
Sean Warne <sean.andrew.warne@gmail.com>
2024-09-14 22:15:03 -0600

    add attack state, update gitignore

--- OakWoods/scripts/player.gd
+++ OakWoods/scripts/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
+const ACCELERATION = 600.0
+const DEACCELERATION = 600.0
 const JUMP_VELOCITY = -300.0
 
 enum {
-	RUN,
+	MOVE,
 	JUMP,
-	FALL
+	FALL,
+	ATTACK
 }
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -21,15 +24,18 @@ func _ready():
 
 func _physics_process(delta):
 	# movement
-	move(delta)
+	if state != ATTACK:
+		move(delta)
 	
 	match state:
-		RUN:
-			run_state()
+		MOVE:
+			move_state()
 		JUMP:
 			jump_state()
 		FALL:
 			fall_state()
+		ATTACK:
+			attack_state(delta)
 
 func move(delta):
 	# gravity
@@ -38,20 +44,23 @@ func move(delta):
 	
 	var direction = Input.get_axis("run_left", "run_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION*delta)
 		if direction < 0:
 			sprite_2d.flip_h = true
 		else:
 			sprite_2d.flip_h = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, DEACCELERATION*delta)
 	
 	if Input.is_action_just_pressed("jump"):
 		state = JUMP
 	
+	if Input.is_action_just_pressed("attack") and is_on_floor():
+		state = ATTACK
+	
 	move_and_slide()
 
-func run_state():
+func move_state():
 	if velocity.x:
 		animation_state.travel("run")
 	else:
@@ -71,4 +80,12 @@ func fall_state():
 		animation_state.travel("fall")
 	
 	if is_on_floor():
-		state = RUN
+		state = MOVE
+
+func attack_state(delta):
+	animation_state.travel("attack")
+	velocity.x = move_toward(velocity.x, 0, DEACCELERATION*delta)
+	move_and_slide()
+
+func attack_finish():
+	state = MOVE
